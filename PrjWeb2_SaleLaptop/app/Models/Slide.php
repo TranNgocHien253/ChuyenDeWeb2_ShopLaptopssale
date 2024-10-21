e2f4c8343614d6fce1127b88913ae6c81b40f175
TranNgocHien253 <161793880+TranNgocHien253@users.noreply.github.com>
2024-10-21 17:09:47 +0700

    n

--- PrjWeb2_SaleLaptop/app/Models/Slide.php
+++ PrjWeb2_SaleLaptop/app/Models/Slide.php
@@ -8,16 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Slide extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
-
-    // Khai báo bảng tương ứng với model
-    protected $table = 'slides';
-
-    // Khai báo các thuộc tính có thể gán
-    protected $fillable = [
-        'link',
-        'image',
-    ];
->>>>>>> Hien_branch_dev
 }
